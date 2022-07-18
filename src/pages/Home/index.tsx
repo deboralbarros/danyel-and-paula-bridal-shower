24131dec432b5fdcfcd0ca2ee6bed43bd5708b71
Débora Barros <debora_lbarros@outlook.com>
2022-07-18 08:25:24 -0400

    change bemol's link

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,7 +10,7 @@ export function Home() {
 
       <h2>Lista de presentes</h2>
       <div>
-        <a href="https://chat.bemol.com.br/pauladanyel" target="_blank" rel="noreferrer">Bemol</a>
+        <a href="http://listadepresente.de/paulinhadanyel" target="_blank" rel="noreferrer">Bemol</a>
         <a href="https://minhahavan.com/lista-de-presentes/627789?idFilial=&barcode=LIS2534796631" target="_blank" rel="noreferrer">Havan</a>
       </div>
     </Container>
